bb6a1891ec1dc4fc6be8859a344fb42a2f61c712
Alibay74 <aitov.74@list.ru>
2020-12-24 21:19:18 -0500

    ddddddddddd

--- src/newClass.java
+++ src/newClass.java
@@ -1,4 +1,13 @@
 
 public class newClass {
-
+ 
+    //             features="/Users/apple/eclipse-workspace/Java Basic/CucumberCRM/src/main/java/Features/Login.feature", 
+    //             glue= {"StepDefinitions"},
+    //             tags="@crm",
+    //             plugin= {"pretty","html:test-output",
+    //                     "html:target/cucumber-default-report",//create basic html report in cpecified
+    //                     "json:target/cucumber.json"
+    // })
+    public class TestRunner {	
+    }
 }
